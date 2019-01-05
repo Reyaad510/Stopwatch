caf568d1c446d88e1a4ac2bb6739f2fdd0853c50
Reyaad Rafeek <reyaad510@gmail.com>
2019-01-05 11:02:12 -0500

    Basic constructor function template made

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('hi');
+
+// const sw = new Stopwatch()
+
+// property
+// Initially 0
+// sw.duration
+// 3 methods
+// Can't call stop or start twice in row, will throw error
+// sw.start
+// sw.stop
+// sw.reset
+
+function Stopwatch() {
+    let duration = 0;
+
+    this.start = function() {
+        
+    };
+    this.stop = function() {
+        
+    };
+    this.reset = function() {
+        
+    };
+
+    Object.defineProperty(this, 'duration', {
+        get: function() {return duration;}
+    })
+}
+
+const sw = new Stopwatch();
